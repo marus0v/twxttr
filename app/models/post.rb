8a4fd542c65676d45732c80e4b82546c2c6fa289
marus0v <andrey.marus@gmail.com>
2025-03-21 06:23:51 +0300

    Comments added, statuses added

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,8 @@
 class Post < ApplicationRecord
+  include Visible
+
+  has_many :comments
+
   validates :title, presence: true
   validates :body, presence: true, length: { minimum: 10 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root "posts#index"
 
-  resources :posts
+  resources :posts do
+    resources :comments
+  end
 
   get "up" => "rails/health#show", as: :rails_health_check
 end
