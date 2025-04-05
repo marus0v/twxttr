bd90b7d02369832e8f79680346f980faa6957929
marus0v <andrey.marus@gmail.com>
2025-04-05 22:03:11 +0300

    System works for registered users, registration failures

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,19 +1,78 @@
 class CommentsController < ApplicationController
+  # before_action :authorize, only: [:create, :edit, :update, :destroy]
+  # before_action :authorize, only: [:create, :destroy]
+  # before_action :set_comment, only: [:show, :edit, :update, :destroy]
+  # before_action :set_comment, only: [:destroy]
+  before_action :authorize, only: [ :create, :destroy ]
+  before_action :set_post, only: [ :create, :destroy ]
+  before_action :set_comment, only: [ :destroy ]
+  before_action :check_comment_owner, only: [ :destroy ]
+
   def create
-    @post = Post.find(params[:post_id])
-    @comment = @post.comments.create(comment_params)
-    redirect_to post_path(@post)
+    @comment = @post.comments.new(comment_params.merge(author_id: current_user))
+
+    if @comment.save
+      redirect_to @post, notice: "Comment was successfully created."
+    else
+      render "posts/show"
+    end
   end
+
+  # def create
+  #  @post = Post.find(params[:post_id])
+  #  @comment = @post.comments.create(comment_params)
+  #  redirect_to post_path(@post)
+  # end
+
+  # def create
+  #  @post = current_user.posts.new(post_params) # Связываем пост с текущим пользователем
+  #
+  #  if @post.save
+  #    redirect_to @post, notice: 'Post was successfully created.'
+  #  else
+  #    render :new, status: :unprocessable_entity
+  #  end
+  # end
 
   def destroy
     @post = Post.find(params[:post_id])
     @comment = @post.comments.find(params[:id])
     @comment.destroy
-    redirect_to post_path(@post), status: :see_other
+    redirect_to post_path(@post), notice: "Comment was successfully deleted.", status: :see_other
   end
 
+  # def destroy
+  #  @comment.destroy
+  #  redirect_to @post, notice: 'Comment was successfully deleted.', status: :see_other
+  # end
+
+  # def destroy
+  #  @post = Post.find(params[:post_id])
+  #  @comment = @post.comments.find(params[:id])
+  #  @comment.destroy
+  #  redirect_to post_path(@post), status: :see_other
+  # end
+
   private
+    # def comment_params
+    #  params.require(:comment).permit(:commenter, :body, :status)
+    # end
+
+    def set_post
+      @post = Post.find(params[:post_id])
+    end
+
+    def set_comment
+      @comment = @post.comments.find(params[:id])
+    end
+
+    def check_comment_owner
+      unless @comment.user == current_user
+        redirect_to @post, alert: "You can only delete your own comments."
+      end
+    end
+
     def comment_params
-      params.require(:comment).permit(:commenter, :body, :status)
+      params.require(:comment).permit(:body, :status)
     end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,7 @@ class Comment < ApplicationRecord
   include Visible
 
   belongs_to :post
+  belongs_to :user
+
+  validates :body, presence: true
 end
